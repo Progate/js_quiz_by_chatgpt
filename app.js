55c49dcd8089eca2704bc0dc3ba61d518d3de9bd
yuki332 <y.arai@prog-8.com>
2024-08-05 14:53:55 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const {getProblem, judge } = require("./openai-test.js");
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.get('/challenge', async (req, res) => {
+  try {
+    const {problem, initial_code, answer_code} = await getProblem();
+    res.render('challenge', {problem, initial_code, answer_code, error: null});
+  } catch (error) {
+    res.render('challenge', {problem: null, initial_code: null, error: "error fetching problem from OpenAI"} );
+  }
+});
+
+app.post('/check', async (req, res) => {
+  try {
+    const {result, comment, answer_code} = await judge(req.body.instruction, req.body.user_code, req.body.answer_code)
+    const user_code = req.body.user_code
+    res.render('check', {result, comment, answer_code, user_code, error: null});
+  } catch (error) {
+    res.render('check', {result:null, comment:null , error: error});
+  }
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
---
+++ openai-test.js
@@ -0,0 +1,62 @@
+const OpenAI = require("openai");
+const dotenv = require("dotenv");
+
+// prep for read data from .env file
+dotenv.config();
+
+// create new OpenAI object
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+// create connection
+async function getProblem() {
+  const message = `
+you will create a practice problem involving basic JavaScript syntax and control structures.
+the output should only include the problem statement.
+ensure the output is in the following format and in JSON format.
+the output will be polite Japanese.
+    {
+      "problem": "",
+      "initial_code": "",
+      "answer_code": ""
+     }
+  `;
+
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: message }],
+    model: "gpt-4o-mini",
+    response_format: {type: "json_object"},
+  });
+
+  const res = completion.choices[0].message;
+  const { problem, initial_code, answer_code} = JSON.parse(res.content);
+  return {problem, initial_code, answer_code};
+}
+
+async function judge(instruction, userCode, answerCode){
+  const message = `
+you will judge the following code. you will check requirements for the code and check if the code fulfill the requirements.
+the code ${userCode}. requirements ${instruction}
+output is JSON. result must be either "正解" or "不正解". you will translate the output into polite Japanese.
+{
+"result": "",
+"comment": "",
+"answer_code": ""
+}
+  `;
+
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: message }],
+    model: "gpt-4o-mini",
+    response_format: {type: "json_object"},
+  });
+
+  const res = completion.choices[0].message;
+  console.log(res.content);
+  const {result, comment, answer_code} = JSON.parse(res.content);
+  return {result, comment, answer_code, userCode};
+}
+// execute function
+module.exports = {getProblem, judge};
+
+
+
